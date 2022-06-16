d78db47b643fd14ceb7561f488e30259477b0aac
Gavinus <96060707+Gavinus@users.noreply.github.com>
2022-06-16 14:21:35 +0200

    recep Page

---
+++ src/components/Recept.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Recept = () => {
+  return (
+    // <div className="reception">
+    //   <div className="receptionBackground">
+    //     <div className="receptionBorder">
+    //       <div className="receptionBloc1">
+    //         <h1>Gavin de Zaltza Portfolio</h1>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    // <div className="reception">
+    <div class="wrapper">
+      <div class="clip-text clip-text_one">
+        <h1>GAVIN DE ZALTZA PORTFOLIO</h1>
+      </div>
+    </div>
+    // </div>
+  );
+};
+
+export default Recept;
